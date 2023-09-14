9c197028f795cd52934f77ce2aff7ba1c9abebec
Udara Chandimal Sathmina <87545327+Chandi-99@users.noreply.github.com>
2023-09-14 12:11:08 +0530

    Add files via upload

---
+++ Practical 3/Practical3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double number1;//declare number 1
+            Double number2;//declare number 2
+            Double average;//declare average of number 1 and number 2
+
+            Console.Write("Enter first integer-");//prompt user
+            //read first number from user
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second integer- ");//prompt user
+            //read second number from user
+            number2 = Convert.ToInt32(Console.ReadLine());
+            average = (number1 + number2) / 2;//average numbers
+            Console.WriteLine("average is {0}",average);//display average
+            Console.Read();
+        }
+    }
+}
---
+++ Practical 4/Practical2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;//declare first number to add
+            int number2;//declare second number to add
+            int number3;//declare third number to add
+            int sum; //declare sum of number 1 , number2 and number 3
+
+            Console.Write("Enter first integer: "); //prompt user
+            // read first number from user
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second integer: ");//prompt user
+           //read second number from user
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third integer ");//prompt user
+            // read third number from user
+            number3 = Convert.ToInt32(Console.ReadLine());
+            sum = number1 + number2 + number3; //add numbers
+            Console.WriteLine("Sum is {0}", sum); //display sum
+            Console.Read();
+        }
+    }
+}
